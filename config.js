2c1606299fbceb7f41cab3adc8f27fcc943f1b67
chenjhyy <55721887+chenjhyy@users.noreply.github.com>
2023-11-05 23:26:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "START",
+        turn_on: "Turn on lights",
         play: "放音乐！",
         bannar_coming: "小横幅",
         balloons_flying: "嘟",
